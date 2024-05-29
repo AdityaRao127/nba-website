0b5a6caf5c91d217865f5759a21c6e23e161f656
Aditya Rao <rao.aditya.codes@gmail.com>
2024-05-29 01:51:00 -0700

    listed k-means clustering results on teams from every season

--- nba-app/analysis/classification/clustering.py
+++ nba-app/analysis/classification/clustering.py
@@ -85,8 +85,70 @@ print("Rebuilding teams: ", rebuilding_teams)
 print("Playoff contendors: ", playoff_contendors)
 print("Champ contendors: ", champ_contendors)
 
+print("Defensive Efficiency and Average Scoring Margin Results")
+## All championship contenders based on defensive efficiency and average scoring margin. The hue represents the win percentage.
+for season in range(2004, 2025):
+    print(f"{season} Season")
+    rebuilding_teams_season = rebuilding_teams.loc[rebuilding_teams['Year'] == season, 'Team']
+    for team in rebuilding_teams_season:
+        print(f"{team} in {season} - Play-in contendors")
+    
+    playoff_contendors_season = playoff_contendors.loc[playoff_contendors['Year'] == season, 'Team']
+    for team in playoff_contendors_season:
+        print(f"{team} in {season} - Playoff contender")
+        
+    champ_contendors_season = champ_contendors.loc[champ_contendors['Year'] == season, 'Team']
+    for team in champ_contendors_season:
+        print(f"{team} in {season} - Championship contender")
+        
+    print("\n")
+    
+##
+##
+##
+#How does effective field goal percentage and opponent effective field goal percentage correlate to winning reg season games?
+x = "Effective Field Goal Percentage"
+y = "Opponent Effective Field Goal Percentage"
 
-for season in (2004, 2025):
+X = merged_df[[x,y]].values
+
+kmeans = KMeans(n_clusters=3).fit(X) 
+# 3 clusters for champ contendors, playoff contendors, rebuilding teams
+
+clusters = kmeans.labels_
+print(clusters) # 0, 1, 2 for each cluster.
+print(merged_df['Win Percentage'].values) # print this, map to cluster values to see what cluster values mean
+
+#0 = rebuild, 1 = champ contendors, 2 = playoff contendors
+
+cluster_df = pd.DataFrame(np.hstack((X, clusters.reshape(-1, 1))), columns=[x, y, "Win Percentage"])
+
+
+sns.scatterplot(x=x, y=y, data=cluster_df, hue="Win Percentage")
+plt.plot()
+plt.show()
+plt.clf()
+
+
+## find teams from each cluster
+merged_df['Cluster'] = clusters
+
+rebuilding_teams = merged_df.loc[merged_df['Cluster'] == 0, ['Team', 'Year']]
+playoff_contendors = merged_df.loc[merged_df['Cluster'] == 2, ['Team', 'Year']]
+champ_contendors = merged_df.loc[merged_df['Cluster'] == 1, ['Team', 'Year']]
+
+#metric_clusters_merged = pd.concat([rebuilding_teams, playoff_contendors, champ_contendors], ignore_index=True)
+
+print("reb", rebuilding_teams.head())
+
+#print("Clustered Teams", cluster_merged_df.head())
+print("Rebuilding teams: ", rebuilding_teams)
+print("Playoff contendors: ", playoff_contendors)
+print("Champ contendors: ", champ_contendors)
+
+print("Effective Field Goal Percentage and Opponent Effective Field Goal Percentage Results")
+## All championship contenders based on effective field goal percentage and opponent effetive field goal percentage. The hue represents the win percentage.
+for season in range(2004, 2025):
     print(f"{season} Season")
     rebuilding_teams_season = rebuilding_teams.loc[rebuilding_teams['Year'] == season, 'Team']
     for team in rebuilding_teams_season:
---
+++ nba-app/analysis/regression/linear_regression.py
@@ -0,0 +1,9 @@
+''' 
+Steps:
+1. define data frame with name, success metric, and whether team won championship or not. 
+
+2. 
+
+
+
+'''
