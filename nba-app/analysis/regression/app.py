491ca92bf4495ff1c4080618217b5968ebc28c48
Aditya Rao <rao.aditya.codes@gmail.com>
2024-06-01 16:33:25 -0700

    small changes

--- nba-app/analysis/regression/app.py
+++ nba-app/analysis/regression/app.py
@@ -7,7 +7,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-port = os.getenv('VITE_APP_PORT')
+port = os.getenv('APP_PORT')
 # Initialize Flask app
 app = Flask(__name__)
 CORS(app)
@@ -15,8 +15,7 @@ CORS(app)
 # Load the combined DataFrame
 df_combined = pd.read_csv('dataframe_combined.csv')
 
-# Load ONNX model
-session = rt.InferenceSession("logistic_regression_model.onnx")
+
 
 def get_success_score(team):
     latest_season = 2024
@@ -37,21 +36,27 @@ def predict():
     data = request.get_json()
     team = data.get("team")
     
-    success_score = get_success_score(team)
+    # Load ONNX model
+    session = rt.InferenceSession("logistic_regression_model.onnx")
+
+    # sucess score
+    latest_season = 2024
+    team_data = df_combined[(df_combined['Team'] == team) & (df_combined['Year'] == latest_season)]
     
-    if success_score is None:
+    if team_data.empty:
         return jsonify({'error': 'Team data not found'}), 404
 
-    # Prepare input for ONNX model
+    success_score = team_data['Success Score'].values[0]
+
     input_name = session.get_inputs()[0].name
     inputs = np.array([[success_score]], dtype=np.float32)
 
     # Make prediction
     probs = session.run(None, {input_name: inputs})
-    win_probability = probs[1][0][1] * 100  # Probability of winning
+    win_probability = probs[1][0][1] * 100  
 
     return jsonify({'team': team, 'win_probability': f"{win_probability:.3f}%"})
 
 if __name__ == '__main__':
     import os
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', port)))
