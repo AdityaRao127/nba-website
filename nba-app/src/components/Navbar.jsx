f65dcc17fb8b65da90afd5ee83bbe9ee528b3264
Brad H <bradhuallpa@gmail.com>
2024-06-07 21:14:04 -0700

    added navbar

--- nba-app/src/components/Navbar.jsx
+++ nba-app/src/components/Navbar.jsx
@@ -3,14 +3,16 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <ul>
-        <li style={{ marginLeft: '15px'}}><Link to="/">Home</Link></li>
-        <li style={{ marginLeft: '15px' }}><Link to="/predictions">Prediction</Link></li>
-        <li style={{ marginLeft: '15px' }}><Link to="/analysis">Team Analysis</Link></li>
-        <li style={{ marginLeft: '15px' }}><Link to="/model-breakdown">Model</Link></li>
-      </ul>
-    </div>
+    <nav>
+        <div className="navbar">
+          <ul>
+            <li style={{ marginLeft: '15px'}}><Link to="/">Home</Link></li>
+            <li style={{ marginLeft: '15px' }}><Link to="/predictions">Prediction</Link></li>
+            <li style={{ marginLeft: '15px' }}><Link to="/analysis">Team Analysis</Link></li>
+            <li style={{ marginLeft: '15px' }}><Link to="/model-breakdown">Model</Link></li>
+          </ul>
+        </div>
+    </nav>    
   );
 };
 
