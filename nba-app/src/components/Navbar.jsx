88e9d7b0c0b1d1e6d29aab5c01756be9e5f10920
Brad H <bradhuallpa@gmail.com>
2024-06-07 22:18:43 -0700

    dont use

--- nba-app/src/components/Navbar.jsx
+++ nba-app/src/components/Navbar.jsx
@@ -3,16 +3,19 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav>
-        <div className="navbar">
-          <ul>
-            <li style={{ marginLeft: '15px'}}><Link to="/">Home</Link></li>
-            <li style={{ marginLeft: '15px' }}><Link to="/predictions">Prediction</Link></li>
-            <li style={{ marginLeft: '15px' }}><Link to="/analysis">Team Analysis</Link></li>
-            <li style={{ marginLeft: '15px' }}><Link to="/model-breakdown">Model</Link></li>
-          </ul>
-        </div>
-    </nav>    
+    <div className='header'>
+      <h1 className='title' style={{ marginLeft: '3px', color: 'white' }}>NBA Prediction Project</h1>
+      <nav>
+          <div className="navbar">
+            <ul>
+              <li style={{ marginLeft: '15px'}}><Link to="/">Home</Link></li>
+              <li style={{ marginLeft: '15px' }}><Link to="/predictions">Prediction</Link></li>
+              <li style={{ marginLeft: '15px' }}><Link to="/analysis">Team Analysis</Link></li>
+              <li style={{ marginLeft: '15px' }}><Link to="/model-breakdown">Model</Link></li>
+            </ul>
+          </div>
+      </nav>    
+    </div>
   );
 };
 
